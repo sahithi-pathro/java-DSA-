8262ca05b28178069cf2b2d1487932882cba019d
sahithi-pathro <sahithipathro@gmail.com>
2026-07-24 13:55:44 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd
+{
+	public static void main(String[] args) {
+	    int a=32;
+	    if(a%2==0){
+	        System.out.println("Even");
+	    }
+	    else{
+	        System.out.println("Odd");
+	    }
+	}
+}
