415354a47bac7e6f6e5c99e9a798fcd0ee564842
sahithi-pathro <sahithipathro@gmail.com>
2026-07-24 14:48:49 +0530

    Add files via upload

---
+++ ArrayLar.java
@@ -0,0 +1,14 @@
+public class ArrayLar{
+    public static void main (String[] args) {
+        int[][] arr={{3,4,5},{5,6,7},{4,7,8}};
+        int largest=arr[0][0];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]>largest){
+                    largest=arr[i][j];
+                }
+            }
+        }
+        System.out.println(largest);
+    }
+}
