89ba33f788b3387451a464692b655b0af83dad39
sahithi-pathro <sahithipathro@gmail.com>
2026-07-24 14:36:11 +0530

    Add files via upload

---
+++ Pascal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pascal{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
