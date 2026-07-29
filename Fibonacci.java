013249a28108b455de3cccc10c31367f275a1373
sahithi-pathro <sahithipathro@gmail.com>
2026-07-29 11:51:32 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 5;
+        int first = 0, second = 1;
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
